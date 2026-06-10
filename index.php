48de9dff609f2ce9af80f5e3438fae6fa3616b29
amrita <mangalarumun1127@gmail.com>
2026-06-10 20:22:47 +0545

    FINALIZED

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 echo '<h1>WELCOME TO BENI!</h1>';
 echo '<h1>WELCOME TO MANGALA MYAGDI!</h1>';
-echo '<h1>Please reduce image size to 500</h1>';
-echo'<img src="mnr.jpg"width="700">';
+echo'<img src="mnr.jpg"width="500">';
 ?>
